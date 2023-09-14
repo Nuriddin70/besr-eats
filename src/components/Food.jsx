58df37a7341f73dae6e4a7671697ac73dea81db2
Nuriddin70 <nuriddin3050@gmail.com>
2023-09-14 20:37:24 +0900

    added some style

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -13,25 +13,31 @@ function Food() {
         <div>
           <p className='font-bold text-gray-700'>Filter Type</p>
           <div>
-            <button>All</button>
-            <button>Burgers</button>
-            <button>Pizza</button>
-            <button>Salads</button>
-            <button>Chicken</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
           </div>
         </div>
 
         {/* Filter Price */}
         <div>
-          <p>Filter Price</p>
-          <div>
-            <button>$</button>
-            <button>$$</button>
-            <button>$$$</button>
-            <button>$$$$</button>
+          <p  className='font-bold text-gray-700'>Filter Price</p>
+          <div >
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
+            <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
           </div>
         </div>
+      </div> 
+
+{/* Display foods */}
+      <div>
+
       </div>
+
     </div>
   )
 }
